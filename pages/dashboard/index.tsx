e73f065095074644d9f865fdfae04bfc4730b18d
Ayush <thapaaayush115@gmail.com>
2022-05-30 09:07:42 +0545

    first commit

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DashTemplate from '../../components/dashboard/templates/DashTemplate'
+
+export default function Index() {
+  return (
+    <div>
+      <DashTemplate>
+<h2>Hello w</h2>
+      </DashTemplate>
+    </div>
+  )
+}
